9dba88de30314a193185a7e99878601e70a4e610
Muu-coder <k2az8u@gmail.com>
2024-11-05 00:04:41 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Test Page</h1>
+      </div>
+    );
+  }
+};
